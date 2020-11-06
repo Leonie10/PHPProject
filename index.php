c284ecdb587c2e350bd31d98f381e3fbe74f8374
Leonie10 <leonie.10@hotmail.fr>
2020-11-06 14:02:17 +0100

    Creating the form style for a basic message chat

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Chat Message</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+    <form action="" method="POST">
+        <input type="text" name="username" class="username" placeholder="Votre pseudo">
+        <input type="text" name="message" placeholder="Ecrivez un message" class="message">
+        <button>Envoyer</button>
+    </form>
+</body>
+</html>
